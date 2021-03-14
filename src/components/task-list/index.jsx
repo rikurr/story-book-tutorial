966bb398d83b9ec5125192f1b2153f3fe9852706
oya riku <56904584+rikurr@users.noreply.github.com>
2021-03-14 23:50:23 +0900

    Add:チュートリアル4の完了

--- src/components/task-list/index.jsx
+++ src/components/task-list/index.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Task } from "../task";
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../../lib/redux";
 
-export const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
+export const PureTaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -57,7 +59,7 @@ export const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
   );
 };
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   /** Checks if it's in loading state */
   loading: PropTypes.bool,
   /** The list of tasks */
@@ -67,6 +69,18 @@ TaskList.propTypes = {
   /** Event to change the task to archived */
   onArchiveTask: PropTypes.func,
 };
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false,
 };
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    ),
+  }),
+  (dispatch) => ({
+    onArchiveTask: (id) => dispatch(archiveTask(id)),
+    onPinTask: (id) => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
